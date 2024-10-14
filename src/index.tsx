f0353281c1590fb530cd6357f9f5bcd90de9fe45
Toshit Chawda <r58playz@gmail.com>
2024-10-13 21:18:58 -0700

    add some css vars (fix up this)

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,20 @@ export class TabsController {
 
 const Tabs: Component<{ controller: TabsApiProxyImpl }, {}> = function() {
 	this.controller = TabsApiProxyImpl.createInstance(() => true);
+	// @ts-ignore DREAMLAND_SECRET_DEV_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED
+	this._leak = true;
+
+	const cssClass = css`
+		background: var(--color-web-ui-tab-strip-background);
+        height: 100%;
+        margin: 0;
+        overflow: hidden;
+        padding: 0;
+        width: 100%;
+	`;
+
 	return (
-		<div>
+		<div class={cssClass}>
 			<tabstrip-tab-list />
 		</div>
 	)
